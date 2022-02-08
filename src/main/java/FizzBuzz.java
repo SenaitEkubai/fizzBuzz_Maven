48a80da952b7ab89e356fe5f3d1b0513264fbab5
senait <senait.ekubai@capgemini.com>
2022-02-08 17:09:19 +0000

    added if statements

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,8 +1,26 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-
+fizzBuzz(15);
+fizzBuzz(3);
+fizzBuzz(5);
+fizzBuzz(17);
     }
     public static String fizzBuzz(int num){
-        return "";
+        if(num % 3 ==0 && num % 5 ==0){
+            System.out.println("FIZZBUZZ");
+            return "FIZZBUZZ";
+        }
+        else if(num % 3 ==0 ){
+            System.out.println("FIZZ");
+            return  "FIZZ";
+        }
+        else if (num % 5 ==0){
+            System.out.println("BUZZ");
+            return  "BUZZ";
+        }
+        else {
+            System.out.println(num);
+            return ""+num;
+        }
     }
 }
