2d7317850c335b4e996b4d81a34a95b33e5e77ac
senait <senait.ekubai@capgemini.com>
2022-02-08 17:30:46 +0000

    created 3 methods

--- src/main/java/FizzBuzzInstances.java
+++ src/main/java/FizzBuzzInstances.java
@@ -1,5 +1,22 @@
+/*
+*GIVEN an input of two numbers
+WHEN the program is run
+THEN report the number of fizz, buzz and fizzbuzz instances found between those two numbers*/
+
 public class FizzBuzzInstances {
     public static void main(String[] args) {
+    }
 
+    public int fizzInstances(int x, int y){
+
+        return 0;
+    }
+    public int buzzInstances(int x, int y){
+
+        return 0;
+    }
+    public int fizzBuzzInstances(int x, int y){
+
+        return 0;
     }
 }
