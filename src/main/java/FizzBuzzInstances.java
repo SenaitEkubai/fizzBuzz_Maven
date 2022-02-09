882c04d159b57fe015bfdc3e21fa2c9196df209e
senait <senait.ekubai@capgemini.com>
2022-02-09 10:48:52 +0000

    implemented report instances into a json file

--- src/main/java/FizzBuzzInstances.java
+++ src/main/java/FizzBuzzInstances.java
@@ -1,24 +1,16 @@
-    /*
-    *GIVEN an input of two numbers
-    WHEN the program is run
-    THEN report the number of fizz, buzz and fizzbuzz instances found between those two numbers
-    * */
+ import org.json.simple.JSONObject;
 
-    import org.json.simple.JSONObject;
-
-    import java.util.ArrayList;
+ import java.io.FileWriter;
+ import java.io.IOException;
 
     public class FizzBuzzInstances {
-        static ArrayList<Integer>  fizzs =new ArrayList();
-        static ArrayList<Integer>  buzzs =new ArrayList();
-        static ArrayList<Integer>  fizzBuzzs =new ArrayList();
-        static ArrayList<Integer>  others =new ArrayList();
         static JSONObject jsonObject = new JSONObject();
-        public static void main(String[] args) {
+
+        public static void main(String[] args) throws IOException {
             fizzInstancesCounter(5,15);
         }
 
-    public static void fizzInstancesCounter(int x, int y){
+    public static void fizzInstancesCounter(int x, int y) throws IOException {
          int min = Math.min(x,y);
          int max = Math.max(x,y);
          int num_fizz=0;
@@ -30,32 +22,36 @@
         while (min < max-1){
             min++;
         if(min % 3 ==0 && min % 5 ==0){
-            fizzBuzzs.add(min);
           num_fizzBuzz++;
         }
         else if(min % 3 ==0){
-            fizzs.add(min);
             num_fizz++;
         }
 
         else if(min % 5 ==0){
-            buzzs.add(min);
             num_buzz++;
         }
        else{
-           others.add(min);
            other++;
         }
       }
             System.out.println("number of fizz= "+ num_fizz + ", number of buzz="+ num_buzz + ", number of fizzbuzz= "+ num_fizzBuzz + ", other= "+other);
-            System.out.println("fizzs= " + fizzs + " buzzs= "+ buzzs+" fizzBuzzs= "+ fizzBuzzs + "other= "+ others);
+
             jsonObject.put("other", other);
             jsonObject.put("fizz",num_fizz);
             jsonObject.put("buzz", num_buzz);
             jsonObject.put("fizzbuzz",num_fizzBuzz);
 
-        System.out.println(jsonObject);
-
-
+        try {
+            FileWriter file = new FileWriter("/Users/senaitekubai/Library/CloudStorage/OneDrive-Capgemini/intellijworkspace/Java-academy/java-fundamentals/fizzBuzz/src/output.json");
+            file.write(jsonObject.toJSONString());
+            file.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
+        System.out.println("JSON file created: "+jsonObject);
     }
+
+    }
+
