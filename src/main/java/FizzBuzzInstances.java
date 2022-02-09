bafef645742e6ca3feb9ec6bf7d4c897b0f5befa
senait <senait.ekubai@capgemini.com>
2022-02-09 09:46:49 +0000

    added methods to return number of fizz,buzz, fizzbuzz and other instances

--- src/main/java/FizzBuzzInstances.java
+++ src/main/java/FizzBuzzInstances.java
@@ -1,22 +1,53 @@
-/*
-*GIVEN an input of two numbers
-WHEN the program is run
-THEN report the number of fizz, buzz and fizzbuzz instances found between those two numbers*/
+    /*
+    *GIVEN an input of two numbers
+    WHEN the program is run
+    THEN report the number of fizz, buzz and fizzbuzz instances found between those two numbers
+    * */
 
-public class FizzBuzzInstances {
-    public static void main(String[] args) {
+    import java.util.ArrayList;
+
+    public class FizzBuzzInstances {
+        public static void main(String[] args) {
+            fizzInstancesCounter(5,15);
+        }
+
+    public static void fizzInstancesCounter(int x, int y){
+
+         ArrayList<Integer>  fizzs =new ArrayList();
+         ArrayList<Integer>  buzzs =new ArrayList();
+         ArrayList<Integer>  fizzBuzzs =new ArrayList();
+         ArrayList<Integer>  others =new ArrayList();
+
+         int min = Math.min(x,y);
+         int max = Math.max(x,y);
+
+         int num_fizz=0;
+         int num_buzz=0;
+         int num_fizzBuzz=0;
+         int other=0;
+
+        while (min < max-1){
+            min++;
+        if(min % 3 ==0 && min % 5 ==0){
+            fizzBuzzs.add(min);
+          num_fizzBuzz++;
+        }
+        else if(min % 3 ==0){
+            fizzs.add(min);
+            num_fizz++;
+        }
+
+        else if(min % 5 ==0){
+            buzzs.add(min);
+            num_buzz++;
+        }
+       else{
+           others.add(min);
+           other++;
+        }
+      }
+            System.out.println("number of fizz= "+ num_fizz + ", number of buzz="+ num_buzz + ", number of fizzbuzz= "+ num_fizzBuzz + ", other= "+other);
+            System.out.println("fizzs= " + fizzs + "buzzs= "+ buzzs+" fizzBuzzs= "+ fizzBuzzs + "other= "+ others);
+
+        }
     }
-
-    public int fizzInstances(int x, int y){
-
-        return 0;
-    }
-    public int buzzInstances(int x, int y){
-
-        return 0;
-    }
-    public int fizzBuzzInstances(int x, int y){
-
-        return 0;
-    }
-}
