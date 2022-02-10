1c7a88998a158d25a52ea503b97a4629b00bc801
senait <senait.ekubai@capgemini.com>
2022-02-10 12:27:42 +0000

    added google code formater and formatted code

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,26 +1,26 @@
 public class FizzBuzz {
-    public static void main(String[] args) {
-fizzBuzz(15);
-fizzBuzz(3);
-fizzBuzz(5);
-fizzBuzz(17);
+
+  public static void main(String[] args) {
+    fizzBuzz(15);
+    fizzBuzz(3);
+    fizzBuzz(5);
+    fizzBuzz(17);
+    fizzBuzz(100);
+  }
+
+  public static String fizzBuzz(int num) {
+    if (num % 3 == 0 && num % 5 == 0) {
+      System.out.println("FIZZBUZZ");
+      return "FIZZBUZZ";
+    } else if (num % 3 == 0) {
+      System.out.println("FIZZ");
+      return "FIZZ";
+    } else if (num % 5 == 0) {
+      System.out.println("BUZZ");
+      return "BUZZ";
+    } else {
+      System.out.println(num);
+      return "" + num;
     }
-    public static String fizzBuzz(int num){
-        if(num % 3 ==0 && num % 5 ==0){
-            System.out.println("FIZZBUZZ");
-            return "FIZZBUZZ";
-        }
-        else if(num % 3 ==0 ){
-            System.out.println("FIZZ");
-            return  "FIZZ";
-        }
-        else if (num % 5 ==0){
-            System.out.println("BUZZ");
-            return  "BUZZ";
-        }
-        else {
-            System.out.println(num);
-            return ""+num;
-        }
-    }
+  }
 }
