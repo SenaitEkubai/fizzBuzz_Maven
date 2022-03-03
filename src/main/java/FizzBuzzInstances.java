7b093087af632e223d02a8d73742436a1cbac2e0
senait <senait.ekubai@capgemini.com>
2022-03-03 13:51:22 +0000

    removed main method

--- src/main/java/FizzBuzzInstances.java
+++ src/main/java/FizzBuzzInstances.java
@@ -1,34 +1,37 @@
+import java.util.HashMap;
 
 public class FizzBuzzInstances {
-
     static int num_fizz = 0;
     static int num_buzz = 0;
     static int num_fizzBuzz = 0;
     static int other = 0;
 
-    public static void main(String[] args) {
-        //  fizzInstancesCounter(0,16);
-    }
+    public static HashMap fizzInstancesCounter(int x, int y) {
 
-    public static void fizzInstancesCounter(int x, int y) {
+        HashMap<String,Integer> result = new HashMap<>();
         int min = Math.min(x, y);
         int max = Math.max(x, y);
-
+        
         while (min < max - 1) {
             min++;
-            if (min % 3 == 0 && min % 5 == 0) {
+            if (min % 15 == 0) {
                 num_fizzBuzz++;
+                result.put("fizzBuzz", num_fizzBuzz);
             } else if (min % 3 == 0) {
                 num_fizz++;
+                result.put("fizz", num_fizz);
+
             } else if (min % 5 == 0) {
                 num_buzz++;
+                result.put("buzz", num_buzz);
             } else {
                 other++;
+                result.put("other", other);
+
             }
         }
-        System.out.println(
-                "number of fizz= " + num_fizz + ", number of buzz=" + num_buzz + ", number of fizzbuzz= " + num_fizzBuzz
-                        + ", other= " + other);
+        return result;
     }
+
 
 }
