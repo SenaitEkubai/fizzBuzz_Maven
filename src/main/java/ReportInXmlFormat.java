25bdea184a86193083a8ad6faaf6e5c7ccdbe6e0
senait <senait.ekubai@capgemini.com>
2022-03-03 13:49:10 +0000

    added try catch block

--- src/main/java/ReportInXmlFormat.java
+++ src/main/java/ReportInXmlFormat.java
@@ -1,23 +1,19 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-
 import java.io.File;
-import java.io.IOException;
+
+
 
 public class ReportInXmlFormat {
-
-    public static void main(String[] args) throws IOException {
-        FizzBuzzInstances.fizzInstancesCounter(0, 16);
-        addReportAndCreateXmlFile();
+    public static void addReportAndCreateXmlFile() {
+       try{
+           ObjectMapper mapper = new XmlMapper();
+           mapper.writeValue(new File("src/result.xml"), new Results());
+           System.out.println("Xml file successfully created ");
+       }catch (Exception e){
+           System.out.println(e.getMessage());
+       }
     }
-
-    public static void addReportAndCreateXmlFile() throws IOException {
-        ObjectMapper mapper = new XmlMapper();
-        mapper.writeValue(new File("src/result.xml"), new Results());
-        System.out.println("Xml file successfully created ");
-    }
-
-
 }
 
 class Results {
@@ -25,6 +21,4 @@ class Results {
     public String buzz = String.valueOf(FizzBuzzInstances.num_buzz);
     public String fizzBuzz = String.valueOf(FizzBuzzInstances.num_fizzBuzz);
     public String other = String.valueOf(FizzBuzzInstances.other);
-
-
 }
